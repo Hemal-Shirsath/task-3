e760c9e612ec0e2b3a9f1cca703197b6e86ecea8
Hemal-Shirsath <hhemalshirsath@gmail.com>
2025-04-03 23:56:12 +0530

    Add files via upload

---
+++ hello five.java
@@ -0,0 +1,8 @@
+public class PrintHello {
+    public static void main(String[] args) {
+        // Using a for loop to print "Hello" five times
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
