4bbaca705e736c74585a1c1f5e063d9af9672639
Hemal-Shirsath <hhemalshirsath@gmail.com>
2025-04-03 23:50:49 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class EvenOdd{
+
+public static void main(String args[]){
+Scanner reader = new Scanner(System.in);
+
+System.out.print("Enter a number: ");
+
+num-in.nextInt();
+
+if (num%2==0)
+
+System.out.println(num+" is an Even");
+
+else
+
+System.out.println(num" is an Odd");
+}
+}
